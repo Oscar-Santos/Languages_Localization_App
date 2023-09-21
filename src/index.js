c9053bb90ebf3c0bc21aae2404b4e532e8d91239
Oscar Santos <oscar303data@gmail.com>
2023-09-20 19:28:32 -0600

    set up detection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+///
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: {
+          "Welcome to React": "Welcome to React and react-i18next"
+        }
+      },
+      fr: {
+        translation: {
+          "Welcome to React": "Bienvenue à React et react-i18next"
+        }
+      }
+    },
+    lng: document.querySelector('html').lang, // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+    detection: {
+      order: ['htmlTag', 'cookie', 'localStorage', 'sessionStorage' , 'path', 'subdomain'],
+    }
+
+  });
+
+function App() {
+  const { t } = useTranslation();
+
+  return <h2>{t('Welcome to React')}</h2>;
+}
+
+///
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +55,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
