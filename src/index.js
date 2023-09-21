cfca73d5c4443b21d456ec6d58b50fe9daa59693
Oscar Santos <oscar303data@gmail.com>
2023-09-21 09:09:54 -0600

    modify cookies

--- src/index.js
+++ src/index.js
@@ -8,38 +8,28 @@ import ReactDOM from 'react-dom/client';
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector) // passes i18n down to react-i18next
+  .use(HttpApi) // passes i18n down to react-i18next
   .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next"
-        }
-      },
-      fr: {
-        translation: {
-          "Welcome to React": "Bienvenue à React et react-i18next"
-        }
-      }
-    }, // if you're using a language detector, do not define the lng option
     fallbackLng: "en",
     detection: {
-      order: ['htmlTag', 'cookie', 'localStorage', 'sessionStorage' , 'path', 'subdomain'],
+      order: [, 'cookie','htmlTag', 'localStorage', 'sessionStorage' , 'path', 'subdomain'],
       caches: ['cookie'],
-    }
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
 
   });
 
 function App() {
   const { t } = useTranslation();
 
-  return <h2>{t('Welcome to React')}</h2>;
+  return <h2>{t('Welcome_to_React')}</h2>;
 }
 
 ///
