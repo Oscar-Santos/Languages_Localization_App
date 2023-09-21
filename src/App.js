bcd02a32e2984aedf03a53ca794285033a5c1952
Oscar Santos <oscar303data@gmail.com>
2023-09-21 10:37:57 -0600

    add dropdown button from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useTranslation } from "react-i18next";
 import './App.css';
 
 function App() {
+
+    const { t } = useTranslation();
+
+    const releaseDate = new Date('2021-03-07');
+    const timeDifference = new Date() - releaseDate
+    const number_of_days = Math.floor(timeDifference / (1000 * 3600 * 24));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="d-flex justify-content-end">
+      <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+
+      </div>
+      <div className='d-flex flex-column align-items-start'>
+        <h1 className="font-weight-normal mb-3">
+          {t('welcome_message')}
+
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t('days_since_release', { number_of_days })  }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-// import App from './App';
+import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 ///
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector) // passes i18n down to react-i18next
   .use(HttpApi) // passes i18n down to react-i18next
   .init({
+    supportedLanguages: ['en', 'fr', 'ar', 'es'],
     fallbackLng: "en",
     detection: {
       order: [, 'cookie','htmlTag', 'localStorage', 'sessionStorage' , 'path', 'subdomain'],
@@ -26,11 +29,11 @@ i18n
 
   });
 
-function App() {
-  const { t } = useTranslation();
+// function App() {
+//   const { t } = useTranslation();
 
-  return <h2>{t('Welcome_to_React')}</h2>;
-}
+//   return <h2>{t('Welcome_to_React')}</h2>;
+// }
 
 ///
 
